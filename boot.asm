0fe05cda911ff92b5f3c3e14d11c971c4a7c1757
dev <you@example.com>
2022-09-14 10:09:39 -0400

    f

--- boot.asm
+++ boot.asm
@@ -4,6 +4,7 @@ section .multiboot               ;according to multiboot spec
         dd 0x1BADB002            ;set magic number for
                                  ;bootloader
         dd 0x0                   ;set flags
+        dd - (0x1BADB002 + 0x0)  ;set checksum
 
 
 section .text
